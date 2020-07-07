049fa2b6cdbc4c6e45d49e7fd87598ed329b3810
zhanghaitao <haitao.zhang@haiziwang.com>
2020-07-07 09:20:35 +0800

    利用代码设置rootViewcontroller

--- Swift_BaseSkill/AppInfo/SceneDelegate.swift
+++ Swift_BaseSkill/AppInfo/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         
-        window?.rootViewController = UINavigationController(rootViewController:HTMainVC())
+         window?.rootViewController = UINavigationController(rootViewController:HTMainVC())
         window?.makeKeyAndVisible()
 
     }
