ab76d6c0f0b019ff1269cc501e4d036770a4a8fa
zhanghaitao <haitao.zhang2@amh-group.com>
2021-02-04 11:20:01 +0800

    Dev分支第一次提交

--- Swift_BaseSkill/AppInfo/AppDelegate.swift
+++ Swift_BaseSkill/AppInfo/AppDelegate.swift
@@ -38,8 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application1(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+        //我是Dev分支的第一次提交；
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
